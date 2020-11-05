ab56e7908ba16b064c93fb7aea8d2cec96fc2d4d
Fernando Caballero <cabafer1@gmail.com>
2020-11-05 08:36:29 -0300

    nuevo comentario

--- proyectobdd04.sql
+++ proyectobdd04.sql
@@ -3,5 +3,30 @@ go
 
 use proyectobdd04;
 go
+--TABLA JUEGO
+create table juego(
+	idDesarrolladora int,
+	idFranquicia int,
+	idJuego int,
+	nombre varchar(50),
+	precio money,
+	idGenero int,
+	editores varchar(50),
+	lanzamiento date,
+	tamanioMB float,
+	etiquetas varchar(25),
+	tipoSO varchar(25),
+	review varchar(50),
+	descripcion varchar(100),
+	constraint PK_juego primary key (idDesarrolladora,idFranquicia,idJuego)
+);	
+go
 
+create table genero(
+	idGenero int,
+	descripcion varchar(25),
+	constraint PK_genero primary key (idGenero)
+);
+go
 
+create table 
