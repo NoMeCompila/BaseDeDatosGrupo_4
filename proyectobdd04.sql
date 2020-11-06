0156d5c1bba5ad5f1982d33b904fafe483217b62
Tomas <tomascollar@gmail.com>
2020-11-05 22:33:25 -0300

    Update proyectobdd04.sql

--- proyectobdd04.sql
+++ proyectobdd04.sql
@@ -29,84 +29,32 @@ create table genero(
 );
 go
 
+create table ventaDetalle(
+	--nroFactura int not null,
+	idVenta int not null,
+	--idDesarrolladora int not null,
+	--idFranquicia int not null,
+	--idJuego int not null,
+	constraint PK_ventaDetalle primary key(idVenta)
+	--Agregar despues las efe kas
 
-
------ SOY ADMIM
-
-
-
---a
- 
-
- ---aAAAAAAAAAAA
-
-create table usuario(
- NomYAp varchar (60) not null,
- email varchar (60) not null,
- fechNac date,
- usuario varchar (30) not null,
- idUsuario int primary key 
- )
-
- -- AGREGAR LA CLAVE FORANEA DESP DE AGREGAR LA TABLA LOGRO
-
- create table LogrosGanados(
- idUsuario int   not null, 
- idLogro int not null,
- descripcion varchar (20) not null
-  CONSTRAINT pk_logrosGanados primary key (idUsuario, idLogro)
-  
- )
-
- create table logro (
- idLogro int primary key, 
- descricpcion varchar (10))
-
-  create table Logros_por_Juego(
-  idLogro int not null, 
-  idJuego int not null,
-  CONSTRAINT pk_Logros_por_Juego primary key (idLogro, idJuego))
-
-  --Agregar las FK desp de idDesarrolladora y idFranquicia
-  
-  
-  
-  
-  -- tabla Juego_en_libreria
-create table juego_en_libreria(
-	idUsuario int not null,
-	idDesarrolladora int not null,
-	idFranquicia int not null,
-	idJuego int not null,
-	hrsJugadas float,
-	constraint PK_juego_en_libreria primary key (idUsuario, idJuego),
 );
 go
 
-create table juegosDeseados(
-	idUsuario int not null,
-	idDesarrolladora int not null,
-	idFranquicia int not null,
-	idJuego int not null,
-	constraint PK_juegosDeseados primary key (idUsuario, idJuego),
+create table ventaCabecera(
+	nroFactura int not null,
+	fechaYHora datetime not null,
+	--idUsuario int not null,
+	--idPago int not null,
+	total money not null,
+	constraint PK_ventaCabecera primary key(nroFactura)
+	--Agregar despues las efe kas
 );
 go
 
-create table pais(
-	idPais int not null,
-	descripcion varchar(25) not null,
-	constraint PK_pais primary key (idPais),
-);
-go
-
-create table provincia(
-	idPais int not null,
-	idProvincia int not null,
+create table TipoPago(
+	idPago int primary key,
 	descripcion varchar(20) not null,
-	constraint PK_provincia primary key (idProvincia),
 );
 go
-
-
-
 
